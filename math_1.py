b8e5f166df55e07035182062cf1166a40679e577
Mohammad <114297179+M-Tinati@users.noreply.github.com>
2023-01-16 20:46:17 +0330

    Add files via upload

---
+++ math_1.py
@@ -0,0 +1,6 @@
+# A program that calculates the base and height of parallelograms.
+
+main = int(input("length of base: "))
+height = int(input('measurement of height: '))
+z = main * height
+print(z)
